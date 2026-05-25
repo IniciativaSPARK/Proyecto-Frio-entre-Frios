11ecc0e8fb3c4ac5fee121229e40bc21fa8e87ce
Lapp4 <pillacapuma@gmail.com>
2026-05-25 15:15:06 -0500

    actualizacion

--- Assets/scriptsJugador.cs
+++ Assets/scriptsJugador.cs
@@ -6,25 +6,45 @@ using System;
 
 public class scriptsJugador : MonoBehaviour
 {
-    public float speed= 10f;
+    public float speed = 10f;
+    [SerializeField] private float vida = 10f;
     private Vector2 Movimientos;
+    private Animator animator;
     public void OnMovimientos(InputAction.CallbackContext context)
     {
-        Movimientos= context.ReadValue<Vector2>();
+        Movimientos = context.ReadValue<Vector2>();
     }
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
     void Update()
     {
-      Movimientosjugador();  
+        Movimientosjugador();
+
+        bool seEstaMoviendo = Movimientos != Vector2.zero;
+        animator.SetBool("isMoving", seEstaMoviendo);
     }
     public void Movimientosjugador()
     {
         Vector3 movement = new Vector3(Movimientos.x, 0f, Movimientos.y);
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+
+        if (movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+        }
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
     }
+
+    public void TakeDamage(float damage)
+    {
+        vida -= damage;
+        Debug.Log("Vida del jugador: " + vida);
+        if(vida <= 0)
+        {
+            Debug.Log("GAME OVER");
+        }
+    }
+
 
 }
